acf0e41080f61d9835e7c283f261af4c40c0ef2d
Niels Vandekeybus <progster@gmail.com>
2021-12-20 15:59:22 +0100

    Creating new mu project

---
+++ config/dispatcher/dispatcher.ex
@@ -0,0 +1,38 @@
+defmodule Dispatcher do
+  use Matcher
+  define_accept_types [
+    html: [ "text/html", "application/xhtml+html" ],
+    json: [ "application/json", "application/vnd.api+json" ]
+  ]
+
+  @any %{}
+  @json %{ accept: %{ json: true } }
+  @html %{ accept: %{ html: true } }
+
+  # In order to forward the 'themes' resource to the
+  # resource service, use the following forward rule:
+  #
+  # match "/themes/*path", @json do
+  #   Proxy.forward conn, path, "http://resource/themes/"
+  # end
+  #
+  # Run `docker-compose restart dispatcher` after updating
+  # this file.
+  match "/concept-schemes/*path", @json do
+    Proxy.forward conn, path, "http://resource/concept-schemes"
+  end
+
+
+  match "/concepts/*path", @json do
+    Proxy.forward conn, path, "http://resource/concepts"
+  end
+
+  match "/public-services/*path", @json do
+    Proxy.forward conn, path, "http://resource/public-services"
+  end
+
+
+  match "/*_", %{ last_call: true } do
+    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  end
+end
